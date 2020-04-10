e729bb4b270c0fe573d8a13a89fef06f9d0fb278
lavana555 <timofeevniki3@gmail.com>
2020-04-10 19:26:38 +0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Registration from "./Registration";
+import WithRouterRecoveryPassContainer from "./RecoveryPass/RecoveryPassContainer";
+import {BrowserRouter, Link, Route} from "react-router-dom";
+import WithRouterNewPassContainer from "./NewPass/NewPassContainer";
+import Loginization from "./Login/Loginization";
+import Users from "./Users/Users";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Time from "./Time/Time";
+import AuthMe from "./Login/AuthMe";
+
+import Color from './Color/Color';
+
+
+
+class App extends React.Component {
+
+
+    state = {
+        flag: false
+
+    }
+    showHedaer = () => {
+        this.setState({
+            flag: !this.state.flag
+
+        })
+    }
+
+
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="App">
+                    <div className="header">
+                        {!this.state.flag &&
+                            <div className="headerbtn" onClick={this.showHedaer}>SHOW HEADER</div>
+                        }
+                        {this.state.flag &&
+
+                      <>
+                          <div className="headerbtn" onClick={this.showHedaer}>COVER HEADER</div>
+                          <ul className="nav">
+                            <li><Link to="login">sig-in</Link></li>
+                            <li><Link to="registration">register</Link></li>
+                            <li><Link to="forgot">forgot</Link></li>
+                            <li><Link to="modals">modals</Link></li>
+                            <li><Link to="time">time</Link></li>
+                            <li><Link to="color">color</Link></li>
+                        </ul>
+
+                      </>
+                        }
+                        <Route exact path='/registration' component={Registration}/>
+                        <Route exact path='/forgot/' component={WithRouterRecoveryPassContainer}/>
+                        <Route exact path='/reset-password/:token' component={WithRouterNewPassContainer}/>
+                        <Route exact path='/login/' component={Loginization}/>
+
+                        <Route exact path='/time/' component={Time}/>
+                        <Route exact path='/authme/' component={AuthMe}/>
+
+                        <Route exact path='/color/' component={Color}/>
+
+                    </div>
+
+                    {/*<Route exact path='/SignIN' component={SignIN}/>*/}
+<Users />
+                    {/*<Registration/>*/}
+                    {/*<RecoveryPassContainer/>*/}
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from "react-redux";
+import store from "./store";
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
+    <BrowserRouter>
   <React.StrictMode>
+      <Provider store={store}>
     <App />
-  </React.StrictMode>,
+      </Provider>
+  </React.StrictMode></BrowserRouter>,
   document.getElementById('root')
 );
 
